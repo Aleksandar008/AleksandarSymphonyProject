59f64b55755660459df6469ffcc9bdecc04b919a
Aleksandar008 <42419589+Aleksandar008@users.noreply.github.com>
2021-11-27 12:43:28 +0100

    Initial commit

---
+++ src/main/java/Utils.java
@@ -0,0 +1,11 @@
+public class Utils {
+    public static String prescriptionId = "";
+
+    public static String getPrescriptionId() {
+        return prescriptionId;
+    }
+
+    public static void setPrescriptionId(String prescriptionId) {
+        Utils.prescriptionId = prescriptionId;
+    }
+}
