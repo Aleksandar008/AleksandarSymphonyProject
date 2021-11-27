290dec9223e72458bf24d86032062d3d3166e5dd
Aleksandar008 <42419589+Aleksandar008@users.noreply.github.com>
2021-11-27 14:04:55 +0100

    Initial commit

---
+++ src/main/java/OpenSymphonyIs.java
@@ -0,0 +1,15 @@
+import io.appium.java_client.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import tests.BaseClass;
+
+public class OpenSymphonyIs extends BaseClass {
+    @Test
+    public void createPDFDocument(){
+        String mail = "andrej.zahariev@yahoo.com";
+        String password = "Balaseviccar15*";
+        String tablica = "KO9292AB";
+
+    }
+}
